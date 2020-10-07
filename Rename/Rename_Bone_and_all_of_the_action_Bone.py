27cab7e9167afc390c492821611faeaad32f04ce
pto8913 <39937503+pto8913@users.noreply.github.com>
2020-10-07 10:18:02 +0900

    Update Rename_Bone_and_all_of_the_action_Bone.py

--- Rename/Rename_Bone_and_all_of_the_action_Bone.py
+++ Rename/Rename_Bone_and_all_of_the_action_Bone.py
@@ -2,12 +2,7 @@ import bpy
 from typing import List, Tuple
 
 rename_bone_armature_name = "hoge"
-""" 
-[
-    (current_bone_name, new_bone_name),
-         ~~
-]
-"""
+
 rename_bone_name_list = [
     ("Tail.006", "Tail"),
     ("Tail.007", "Tail.001"),
@@ -25,11 +20,10 @@ armature = get_object(rename_bone_armature_name)
 bpy.ops.object.mode_set(mode = "EDIT")
 
 rev_rename_bone_name_list = []
-# Use this if you have manually changed the bone name of the action up to the halfway point.
 def Init_rev_bone_name_list():
     return [(b, a) for a, b in rename_bone_name_list]
-    
-# rev_rename_bone_name_list = Init_rev_bone_name_list()
+
+rev_rename_bone_name_list = Init_rev_bone_name_list()
 
 def rename_bones():
     __rename_bones(rev_rename_bone_name_list)
